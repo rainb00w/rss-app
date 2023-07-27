d8c3257ec355b73b72a41a89bfbc9db436823f3d
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-27 21:59:27 +0300

    _v1g

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Port - ${port}`));
 
@@ -20,40 +16,43 @@ const URL_API = `https://api.telegram.org/bot${token}/sendMessage`;
 let mainFeed = [[], []];
 let mainFeedDOU = [[], []];
 
-setInterval(async () => {
-  console.log('receiving data');
-  data = await parser.parseURL(
-    'https://www.upwork.com/ab/feed/topics/rss?securityToken=b19c102ae4e12f8d28ba6c1e3fe58816ad134950ff988993af4a4a5b801ab3f594bb2f4bf3b82ece28039fdd66b39fd9924d35c2f4463c18acdc735b99aabf59&userUid=569918438172733440&orgUid=569918438181122049'
-  );
-
-  newFeed = [];
-  data &&
-    data.items.forEach((item) =>
-      newFeed.push([item.title, item.link, item.contentSnippet])
+app.get('/', function (req, res) {
+  setInterval(async () => {
+    console.log('receiving data');
+    data = await parser.parseURL(
+      'https://www.upwork.com/ab/feed/topics/rss?securityToken=b19c102ae4e12f8d28ba6c1e3fe58816ad134950ff988993af4a4a5b801ab3f594bb2f4bf3b82ece28039fdd66b39fd9924d35c2f4463c18acdc735b99aabf59&userUid=569918438172733440&orgUid=569918438181122049'
     );
 
-  const receivedFeed = newFeed.slice(0, 5);
+    newFeed = [];
+    data &&
+      data.items.forEach((item) =>
+        newFeed.push([item.title, item.link, item.contentSnippet])
+      );
 
-  const flattenedMainFeed = mainFeed.flat();
+    const receivedFeed = newFeed.slice(0, 5);
 
-  if (receivedFeed[0][0] !== mainFeed[0][0]) {
-    console.log('--------- here');
-    async function feedIteration(array) {
-      for (const item of array) {
-        let messageBody = `${item[0]} \n ${item[2]} \n ${item[1]}`;
+    const flattenedMainFeed = mainFeed.flat();
 
-        if (!flattenedMainFeed.includes(item[0])) {
-          axios.post(URL_API, {
-            chat_id: CHAT_ID,
-            parse_mode: 'html',
-            disable_web_page_preview: true,
-            text: messageBody,
-          });
+    if (receivedFeed[0][0] !== mainFeed[0][0]) {
+      console.log('--------- here');
+      async function feedIteration(array) {
+        for (const item of array) {
+          let messageBody = `${item[0]} \n ${item[2]} \n ${item[1]}`;
+
+          if (!flattenedMainFeed.includes(item[0])) {
+            axios.post(URL_API, {
+              chat_id: CHAT_ID,
+              parse_mode: 'html',
+              disable_web_page_preview: true,
+              text: messageBody,
+            });
+          }
         }
       }
-    }
 
-    feedIteration(receivedFeed);
-  }
-  mainFeed = receivedFeed;
-}, 60000);
+      feedIteration(receivedFeed);
+    }
+    mainFeed = receivedFeed;
+  }, 60000);
+  res.send('Hello World');
+});
